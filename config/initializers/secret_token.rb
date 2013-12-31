d67606d88cc49c79c4e02cf55b860ca8111327f1
ericproctor <eric.proctor@outlook.com>
2013-12-31 06:58:33 -0500

    first install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pvha4h::Application.config.secret_key_base = 'b096fcd36c53e79b9db23a7a65fc4be1eea07b9131553ce87a720bd4940107852898abfc54393af430634700b3bc64b1df852f3f76b36bbcca208bd857f4dc61'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pvha4h::Application.config.session_store :cookie_store, key: '_pvha-4h_session'
